f4542413d12cc99265065db5e7e22a444b8314e0
kelnid <1Qe3wsss4as>
2021-11-24 14:10:31 +0200

    new branch first commit

---
+++ script/script2.js
@@ -0,0 +1,27 @@
+// classwork quest1
+let message = "Palmo";
+let message2 = 'Hello, Palmo';
+alert(`Hello, ${message}`);
+alert(message2)
+//classwork quest2
+let name = prompt("Как вас зовут?", "");
+alert(`Вас зовут ${name}`);
+//classwork quest3
+var isYou = confirm("Кто вы?");
+alert( isYou );
+//classwork quest4
+let str = "12345";
+let num = Number(str);
+console.log(typeof num);
+//classwork quest5
+let value = true;
+value = String(value); // теперь value это строка "true"
+console.log(typeof value);
+//classwork quest6
+alert( Boolean(null) ); // false
+alert( Boolean(1) ); // true
+//classwork quest7
+let nullStroke = " ";
+alert( Boolean(nullStroke) );
+
+
